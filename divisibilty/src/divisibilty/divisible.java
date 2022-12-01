8eaacee1985325d3b7b82a213e724464d41ccd8d
Bindushree .T.N <112746184+Bindushreetn@users.noreply.github.com>
2022-12-01 19:48:22 +0530

    First commit

---
+++ divisibilty/src/divisibilty/divisible.java
@@ -0,0 +1,19 @@
+package divisibilty;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class divisible {
+
+	public static void main(String[] args) {
+		List <Integer> list=Arrays.asList(12,20,35,46,55,68,75);
+		int result=0;
+		for(int i:list) {
+			if(i%5==0) {
+				result +=i;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
